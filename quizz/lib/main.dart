dcbd6a924db2218dcca18dc3836829d7418ebe9f
hftamayo <hftamayo@gmail.com>
2021-03-29 19:15:42 -0600

    basic main class coding

--- quizz/lib/main.dart
+++ quizz/lib/main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart'
+
 void main() {}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Man of Sorrow'),);
+  }
+}
