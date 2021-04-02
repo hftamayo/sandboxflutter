e949f522e0dd1a2942d218fd1d30adfa84432949
hftamayo <hftamayo@gmail.com>
2021-04-02 12:35:20 -0600

    firstapp: sending a hello message

--- quizz/lib/main.dart
+++ quizz/lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Man of Sorrow'),);
+    return MaterialApp(
+      home: Text('Man of Sorrows'),
+    );
   }
 }
