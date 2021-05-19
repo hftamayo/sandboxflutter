48a5f65d258b0417b932d570c06b8ab575430370
hftamayo <hftamayo@gmail.com>
2021-05-19 14:17:38 -0600

    adding stateful and private props

--- quizz/lib/main.dart
+++ quizz/lib/main.dart
@@ -4,15 +4,32 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion(){
-    print('Answer chosen!');
+//lesson36: class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+//el underscore define una clase como privada
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = ['What\'s your favorite color?', 
-    'What\'s your favorite animal?',];
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -21,19 +38,25 @@ class MyApp extends StatelessWidget {
         //body: Text('This is my default text!'),
         body: Column(
           children: [
-            Text('The question!'),
-            RaisedButton(child: Text('Answer 1'), 
-            onPressed: answerQuestion,
+            Text(questions.elementAt(_questionIndex)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
             ),
-            RaisedButton(child: Text('Answer 2'), 
-            onPressed: answerQuestion,
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Anonymous funct example'),
             ),
-            RaisedButton(child: Text('Answer 3'), 
-            onPressed: answerQuestion,
-            ),            
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //esta es una func anom larga
+                print('Anom funct larga');
+              },
+            ),
           ],
         ),
       ),
     );
   }
-}
+} //fin de class MyAppState
