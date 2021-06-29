0adbda25bbc56a25d7a4b49cf2e16aae3f4dbc28
hftamayo <hftamayo@gmail.com>
2021-06-28 19:15:26 -0600

    showing widgets conditionally

--- quizz/lib/main.dart
+++ quizz/lib/main.dart
@@ -3,13 +3,11 @@ import 'package:flutter/material.dart';
 import './question.dart';
 import './answer.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 //lesson36: class MyApp extends StatelessWidget {
 class MyApp extends StatefulWidget {
-  @override
+    @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MyAppState();
@@ -18,38 +16,44 @@ class MyApp extends StatefulWidget {
 
 //el underscore define una clase como privada
 class _MyAppState extends State<MyApp> {
-  int _questionIndex = 0;
+    final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What\'s your favorite music?',
+      'answers': ['reggue', 'hip hop', 'classic', 'country'],
+    },
+  ];
+
+  var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print("we have more questions!");
+    } else {
+      print("no more questions!");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What\'s your favorite music?',
-        'answers': ['reggue', 'hip hop', 'classic', 'country'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
         ),
         //body: Text('This is my default text!'),
-        body: Column(
+        body: _questionIndex < questions.length ? Column(
           children: [
             Question(
               questions[_questionIndex]['questionText'],
@@ -59,7 +63,7 @@ class _MyAppState extends State<MyApp> {
               return Answer(_answerQuestion, answer);
             }).toList()
           ],
-        ),
+        ) : Center(child: Text('you did it')), 
       ),
     );
   }
